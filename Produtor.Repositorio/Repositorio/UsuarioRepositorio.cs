305a2908d4a648e5a1b0e10408c70b1a952023f4
Ivo <ivofvilla@hotmail.com>
2023-12-20 20:24:25 -0300

    Conctando ao RabbitMQ

--- Produtor.Repositorio/Repositorio/UsuarioRepositorio.cs
+++ Produtor.Repositorio/Repositorio/UsuarioRepositorio.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using Produtor.Modelo;
 using Produtor.Repositorio.Interfaces;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
 
 namespace Produtor.Repositorio.Repositorio
 {
@@ -18,10 +21,40 @@ namespace Produtor.Repositorio.Repositorio
 
         public async Task AdicionarAsync(Usuario usuario, CancellationToken cancellationToken = default)
         {
+            var servidor = _configuration.GetSection("MassTransit")["Servidor"] ?? string.Empty;
             var nomeFila = _configuration.GetSection("MassTransit")["NomeFila"] ?? string.Empty;
-            var endPoint = await _bus.GetSendEndpoint(new Uri($"queue:{nomeFila}"));
+            var userName = _configuration.GetSection("MassTransit")["Usuario"] ?? string.Empty;
+            var Password = _configuration.GetSection("MassTransit")["Senha"] ?? string.Empty;
 
-            await endPoint.Send(usuario);
+            var f = new ConnectionFactory { 
+                HostName = servidor,
+                UserName = userName,
+                Password = Password
+            };
+
+            using (var connection = f.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                            queue: nomeFila,
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+                    var msg = JsonSerializer.Serialize(usuario);
+
+                    var body = Encoding.UTF8.GetBytes(msg);
+
+                    channel.BasicPublish(
+                            exchange: "",
+                            routingKey: nomeFila,
+                            basicProperties: null,
+                            body: body
+                        );
+                }
+            }
         }
     }
 }
